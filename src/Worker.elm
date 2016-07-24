2947f13658ebb4c6951fc8cca24941ce8ea16d57
Luke Westby <lwestby@alumni.nd.edu>
2016-07-24 11:13:00 -0500

    allow all workers to accept extra cmds to wrap update with

--- src/Worker.elm
+++ src/Worker.elm
@@ -1,11 +1,41 @@
-module Worker exposing (workerWithFlags, worker)
+module Worker exposing (workerWithFlags, worker, beginnerWorker)
 
 {-| Start Elm applications without a view
 
-@docs workerWithFlags, worker
+@docs workerWithFlags, worker, beginnerWorker
 -}
 
 import VirtualDom
+
+
+wrapUpdate :
+    (model -> Cmd msg)
+    -> (msg -> model -> ( model, Cmd msg ))
+    -> (msg -> model -> ( model, Cmd msg ))
+wrapUpdate extraCmds innerUpdate =
+    \msg model ->
+        let
+            ( nextModel, nextCmd ) =
+                innerUpdate msg model
+        in
+            ( nextModel
+            , Cmd.batch [ nextCmd, extraCmds nextModel ]
+            )
+
+
+wrapInit :
+    (model -> Cmd msg)
+    -> (flags -> ( model, Cmd msg ))
+    -> (flags -> ( model, Cmd msg ))
+wrapInit extraCmds innerInit =
+    \flags ->
+        let
+            ( nextModel, nextCmd ) =
+                innerInit flags
+        in
+            ( nextModel
+            , Cmd.batch [ nextCmd, extraCmds nextModel ]
+            )
 
 
 {-| Start a worker program with flags from the outside world
@@ -20,16 +50,17 @@ import VirtualDom
     ```
 -}
 workerWithFlags :
-    { init : flags -> ( model, Cmd msg )
-    , update : msg -> model -> ( model, Cmd msg )
-    , subscriptions : model -> Sub msg
-    }
+    (model -> Cmd msg)
+    -> { init : flags -> ( model, Cmd msg )
+       , update : msg -> model -> ( model, Cmd msg )
+       , subscriptions : model -> Sub msg
+       }
     -> Program flags
-workerWithFlags stuff =
+workerWithFlags extraCmds { init, update, subscriptions } =
     VirtualDom.programWithFlags
-        { init = stuff.init
-        , update = stuff.update
-        , subscriptions = stuff.subscriptions
+        { init = wrapInit extraCmds init
+        , update = wrapUpdate extraCmds update
+        , subscriptions = subscriptions
         , view = \_ -> VirtualDom.text ""
         }
 
@@ -37,14 +68,31 @@ workerWithFlags stuff =
 {-| Start a worker program
 -}
 worker :
-    { init : ( model, Cmd msg )
-    , update : msg -> model -> ( model, Cmd msg )
-    , subscriptions : model -> Sub msg
-    }
+    (model -> Cmd msg)
+    -> { init : ( model, Cmd msg )
+       , update : msg -> model -> ( model, Cmd msg )
+       , subscriptions : model -> Sub msg
+       }
     -> Program Never
-worker stuff =
-    workerWithFlags
-        { init = \_ -> stuff.init
-        , update = stuff.update
-        , subscriptions = stuff.subscriptions
+worker extraCmds { init, update, subscriptions } =
+    workerWithFlags extraCmds
+        { init = \_ -> init
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+{-| Start a worker program with just a model a simpler update function
+-}
+beginnerWorker :
+    (model -> Cmd msg)
+    -> { model : model
+       , update : msg -> model -> model
+       }
+    -> Program Never
+beginnerWorker extraCmds { model, update } =
+    worker extraCmds
+        { init = ( model, Cmd.none )
+        , update = \msg model -> ( update msg model, Cmd.none )
+        , subscriptions = \_ -> Sub.none
         }
